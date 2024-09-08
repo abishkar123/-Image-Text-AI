7cf203aca4622ecb49ad51c03af0959afe768ff7
abishkar rai <raiabishkar0.5@gmail.com>
2024-09-08 11:44:53 +1000

    cors updated

--- src/App.jsx
+++ src/App.jsx
@@ -35,6 +35,7 @@ function App() {
       const options = {
         method: "POST",
         body: formData,
+        mode: 'no-cors',
       };
       const response = await fetch(`${rootUrl}/upload`,options);
       const data = await response.json();
